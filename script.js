5b1e3089b94e23acb83518f13a54ee996e08d4b6
BalameenaN <balameena.navukkarasu@gmail.com>
2024-12-30 00:34:26 -0700

    code changes

--- script.js
+++ script.js
@@ -45,89 +45,104 @@ let inputValue = [{
 ];
 
 //getting random country as question
-let randomNo = Math.floor(Math.random() * 11);
-console.log(randomNo);
 
-let countryEl = document.querySelector(".country");
-console.log(countryEl);
-countryEl.textContent = inputValue[randomNo].country;
 
-//getting random capital as options
 let op1 = document.querySelector(".option1");
 let op2 = document.querySelector(".option2");
 let op3 = document.querySelector(".option3");
 let op4 = document.querySelector(".option4");
-
 let options = [op1, op2, op3, op4];
-//console.log(options.length);
-
-let randomArray=[];
-randomArray.push(randomNo);//pushing index of answer into the array
-console.log(randomArray.length);
-do{
-    let no1 = Math.floor(Math.random() * 10);
-    if(randomArray.includes(no1)){
-       continue;
-    }
-    else{
-        randomArray.push(no1);  //pushing index of other options
-    }
-
-}while(randomArray.length<options.length)
-       
-
-//console.log(randomArray);
-
-randomArray.sort((a,b)=> a-b);//to shuffle the option values
-
-console.log(randomArray);
-
-for(let i=0; i < options.length; i++){
-    let input = randomArray[i];
- options[i].textContent = inputValue[input].capital;
-}
-
-/*let totalQuestions = 10;
-for(let i=1; i<=totalQuestions; i++){
-    if (i%2==0){
-        console.log("player2 need to play");
-        
-    }
-}*/
-
+let countryEl = document.querySelector(".country");
 let optionEl = document.querySelector("#question");
 let seperateBtn = document.querySelectorAll("#option");
 let btnEl = document.querySelector("#next");
+let startEl = document.querySelector("#start");
 let answer = document.querySelector("#ans");
 let s = document.querySelector(".sentence");
+let num = Math.floor(Math.random() * 11);
+let totalQuestions = 10;
 
 
-btnEl.addEventListener('click',function(e){
-    btnEl.textContent = "Next";
+//getting random capital as options
 
-})
+//console.log(options.length);
+function valueGenerator(random) {
+    //console.log(random);
+    countryEl.textContent = inputValue[random].country;
+    let randomArray = [];
+    randomArray.push(random);//pushing index of answer into the array
+    console.log(randomArray.length);
+    do {
+        let no1 = Math.floor(Math.random() * 10);
+        if (randomArray.includes(no1)) {
+            continue;
+        }
+        else {
+            randomArray.push(no1);  //pushing index of other options
+        }
+    } while (randomArray.length < options.length)
+    //console.log(randomArray);
 
-optionEl.addEventListener('click',function(e){
-    console.log(e.target.textContent);
-    console.log(seperateBtn);
-    if(e.target.textContent == inputValue[randomNo].capital){
-        console.log("inside if");
-        e.target.style.background = "green";
-        seperateBtn.forEach(function(b){
-            if(e.target != b){
-            b.disabled= true;}
-    })
-    answer.textContent = "CORRECT ANSWER!";
-} 
-    else{
-        e.target.style.background = "red";
-        seperateBtn.forEach(function(b){
-            if(e.target != b){
-            b.disabled= true;}
-    })
-    s.textContent = "Correct Answer is:";
-    answer.textContent = inputValue[randomNo].capital;
+    randomArray.sort((a, b) => a - b);//to shuffle the option values
+
+    console.log(randomArray);
+
+    for (let i = 0; i < options.length; i++) {
+        let input = randomArray[i];
+        options[i].textContent = inputValue[input].capital;
     }
-    
+}
+
+function eventListener(num) {
+    //optionEl.classList.add('active');
+    optionEl.addEventListener('click', function (e) {
+        console.log(e.target.textContent);
+        console.log(seperateBtn);
+       // if(optionEl.classList.contains('active')){
+        if (e.target.textContent == inputValue[num].capital) {
+            console.log("inside if");
+            e.target.style.background = "green";
+            seperateBtn.forEach(function (b) {
+                if (e.target != b) {
+                    b.disabled = true;
+                }
+            })
+            answer.textContent = "CORRECT ANSWER!";
+        }
+        else {
+            e.target.style.background = "red";
+            seperateBtn.forEach(function (b) {
+                if (e.target != b) {
+                    b.disabled = true;
+                }
+            })
+            s.textContent = "Correct Answer is:";
+            answer.textContent = inputValue[num].capital;
+        }
+   // }
+
+    })
+}
+
+
+
+valueGenerator(num);
+eventListener(num);
+btnEl.addEventListener('click', function (e) {
+    let randomNo = Math.floor(Math.random() * 11);
+    //optionEl.classList.remove('active');
+   
+    totalQuestions--;
+    if(totalQuestions>0){
+        seperateBtn.forEach(function (b) {
+            b.disabled = false;
+            b.style.background="rgb(198, 205, 227)";
+    })
+        valueGenerator(randomNo);
+    eventListener(randomNo);
+    }  
 })
+
+
+//function for listening the event
 
