ec80c0bfd672549c5bf4df58677828abaa0c3753
BalameenaN <balameena.navukkarasu@gmail.com>
2024-12-30 19:53:28 -0700

    option selection check

--- script.js
+++ script.js
@@ -44,19 +44,7 @@ let inputValue = [{
 }
 ];
 
-let playerPoints = [
-    {
-        player: 1,
-        point: 0
-    },
-    {
-        player: 2,
-        point: 0
-    }
-];
-
 //getting random country as question
-
 
 let op1 = document.querySelector(".option1");
 let op2 = document.querySelector(".option2");
@@ -74,21 +62,24 @@ let leftEl = document.querySelector("#player1");
 let rightEl = document.getElementById("player2");
 let qno = document.querySelector(".heading");
 let results = document.querySelector("#result")
+let message = document.getElementById("msg");
+let playerTurn = document.querySelector("#result");
 let num1 = Math.floor(Math.random() * 11);
 let totalQuestions = 1;
 let player1 = 0;
 let player2 = 0;
 let ans = 1;
+let isClicked = false;
 
+// initial values while loading the page
 valueGenerator(num1);
-eventListener(num1);
+var num = num1;
 console.log("call1");
 qno.textContent = `QUESTION NO. ${totalQuestions}`
-//getting random capital as options
 
-//console.log(options.length);
+// function to generate random country and capital
 function valueGenerator(random) {
-    //console.log(random);
+
     countryEl.textContent = inputValue[random].country;
     let randomArray = [];
     randomArray.push(random);//pushing index of answer into the array
@@ -102,7 +93,6 @@ function valueGenerator(random) {
             randomArray.push(no1);  //pushing index of other options
         }
     } while (randomArray.length < options.length)
-    //console.log(randomArray);
 
     randomArray.sort((a, b) => a - b);//to shuffle the option values
 
@@ -120,94 +110,104 @@ function pointCounter(a) {
     console.log(a, "a");
     if (totalQuestions % 2 != 0 && a == 1) {
         player1++;
-        console.log(totalQuestions, "pointer1-call6");
+        //console.log(totalQuestions, "pointer1-call6");
     }
     else if (totalQuestions % 2 == 0 && a == 1) {
         player2++;
-        console.log(totalQuestions, "pointer2-call6");
+        //console.log(totalQuestions, "pointer2-call6");
     }
 
 }
+//Adding EventListener for the options through event bubbling
+optionEl.addEventListener('click', function (e) {
+    isClicked = true;
 
-function eventListener(num) {
-
-    optionEl.addEventListener('click', function (e) {
-
-        console.log("call2");
-        console.log(e.target.textContent);
-        console.log(inputValue[num].capital);
-        console.log(num, "num");
-
-        if (e.target.textContent == inputValue[num].capital) {
-            console.log("inside if-call5");
-            e.target.style.background = "green";
-            seperateBtn.forEach(function (b) {
-                if (e.target != b) {
-                    b.disabled = true;
-                }
-            })
-            s.textContent = "";
-            answer.textContent = "CORRECT ANSWER!";
-            ans = 1;
-        }
-        else {
-            e.target.style.background = "red";
-            seperateBtn.forEach(function (b) {
-                if (e.target != b) {
-                    b.disabled = true;
-                }
-            })
-            s.textContent = "Correct Answer is:";
-            answer.textContent = inputValue[num].capital;
-            ans = 0;
-            console.log("inside else-call5");
-        }
-
-        pointCounter(ans);
-        leftEl.textContent = player1;
-        rightEl.textContent = player2;
-        console.log(player1, "player1");
-        console.log(player2, "player2");
-        //num=0;
-
-    })
-}
-
-
-btnEl.addEventListener('click', function (e) {
-    let randomNo = Math.floor(Math.random() * 11);
-    //optionEl.classList.remove('active');
-    console.log(randomNo);
-    console.log("call3");
-    totalQuestions++;
-    qno.textContent = `QUESTION NO. ${totalQuestions}`
-    if (totalQuestions <= 6) {
+    if (e.target.textContent == inputValue[num].capital) {
+        //console.log("inside if-call5");
+        e.target.style.background = "green";
         seperateBtn.forEach(function (b) {
-            b.disabled = false;
-            b.style.background = "rgb(198, 205, 227)";
+            if (e.target != b) {
+                b.disabled = true;
+            }
         })
         s.textContent = "";
-        answer.textContent = "";
-        console.log("call4", randomNo);
-        valueGenerator(randomNo);
-        eventListener(randomNo);
+        answer.textContent = "CORRECT ANSWER!";
+        ans = 1;
     }
     else {
-        btnEl.disabled = true;
-        s.textContent = "";
-        answer.style.color = "red";
-        answer.textContent = "GAME OVER";
+        e.target.style.background = "red";
+        seperateBtn.forEach(function (b) {
+            if (e.target != b) {
+                b.disabled = true;
+            }
+        })
+        s.textContent = "Correct Answer is:";
+        answer.textContent = inputValue[num].capital;
+        ans = 0;
+        //console.log("inside else-call5");
     }
+    // counting the number of points
+    pointCounter(ans);
+    leftEl.textContent = player1;
+    rightEl.textContent = player2;    
 
 })
 
-/*// determining the winner
-if(player1 > player2){
-    results.textContent = "PLAYER 1 is the winner";
-}
-else{
-   results.textContent = "PLAYER 2 is the winner"; 
-}*/
+//adding EventListener for NEXT button
+btnEl.addEventListener('click', function (e) {
+    let randomNo = Math.floor(Math.random() * 11);
+    //console.log(randomNo);
+    //console.log("call3");
+    //main if loop to check if any option is selected or not
+    if (isClicked == true) {
+        totalQuestions++;
+        qno.textContent = `QUESTION NO. ${totalQuestions}`;
+        //nested if to check if the chance has reached maximum no.
+        if (totalQuestions <= 6) {
+
+            seperateBtn.forEach(function (b) {
+                b.disabled = false;
+                b.style.background = "rgb(198, 205, 227)";
+            })
+            s.textContent = "";
+            answer.textContent = "";
+            console.log("call4", randomNo);
+            valueGenerator(randomNo);
+            //eventListener(randomNo);
+            num = randomNo;
+            isClicked = false;
+        }
+        //nested loop else
+        else {
+            btnEl.disabled = true;
+            s.textContent = "";
+            answer.style.color = "red";
+            answer.textContent = "GAME OVER!";
+            qno.style.color = "red";
+            qno.textContent = "GAME OVER!"
+            message.style.color = "red";
+            //Determining the winner
+            if (totalQuestions == 7 && player1 > player2) {
+                message.innerHTML = "<h2>PLAYER 1 is the winner</h2>";
+            }
+            else if (totalQuestions == 7 && player1 < player2) {
+                //results.textContent = "PLAYER 2 is the winner"; 
+                message.innerHTML = "<h2>PLAYER 2 is the winner</h2>";
+            }
+            else if (totalQuestions == 7 && player1 == player2) {
+                //results.textContent = "It's a TIE";
+                message.innerHTML = "<h2>It's a TIE</h2>";
+            }
+        }
+    }
+    //main loop else
+    else {
+        alert("Please select an option before clicking NEXT");
+    }
+})
+
+
+
 
 
 
